13e85dec87e80918df97ca3a9373fe87267ce264
Andrii Diadyk <andriy.dyadyk@gmail.com>
2022-03-24 17:40:46 +0200

    Add some files to Lerning k8s&docker

---
+++ dockerfiles/index.php
@@ -0,0 +1,8 @@
+<?php
+$ip_server = $_SEVER['SERVER_ADDR'];
+
+echo "<h1>Hello from Kubernetes</h1><br>";
+echo "Server Ip Adress is: $ip_server", "<br><p>";
+echo "Made by <font color=green>Diadyk Adnrii"
+
+?>
