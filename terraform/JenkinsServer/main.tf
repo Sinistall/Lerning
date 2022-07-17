7cbe185e01ddb7050c6f35ae08a9256d86ad47c9
Andrii Diadyk <andriy.dyadyk@gmail.com>
2022-07-17 16:48:57 +0300

    Demo version

---
+++ terraform/JenkinsServer/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_security_group" "jenkins-ssh" {
+    name = "Jenkins-SG"
+    dynamic "ingress" {
+      for_each = ["8080", "22"]
+      content {
+        from_port        = ingress.value
+        to_port          = ingress.value
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+      }
+    }
+    egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "ServerJenkins" {
+    ami = "ami-065deacbcaac64cf2"
+    instance_type = "t2.micro"
+    key_name = "keyfrankfurt"
+    security_groups = [aws_security_group.jenkins-ssh.id]
+    user_data = file("user_data.sh")
+
+    tags = {
+      Name = "ServerETC"
+      Terraform = "True"
+    }
+}
+
+output "public_ip_instance" {
+  value = aws_instance.ServerJenkins.public_ip
+}
---
+++ terraform/JenkinsServer/user_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+yum -y update
+yum -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
