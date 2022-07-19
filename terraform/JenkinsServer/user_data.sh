19dac5284ea759464b275f15bf064cb420f523f7
Andrii Diadyk <andriy.dyadyk@gmail.com>
2022-07-19 18:46:15 +0300

    Add block that start container after reboot

--- terraform/JenkinsServer/user_data.sh
+++ terraform/JenkinsServer/user_data.sh
@@ -2,4 +2,14 @@
 apt-get -y update
 apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin 
 apt install docker.io curl
-docker run -p 8080:8080 --name=jenkins-master -d jenkins/jenkins
+
+#If you need to terminate instance and after start, you should create new image and push it to dockerhub, after run container with this image
+docker run -p 8080:8080 --name=jenkins-master -d jenkins/jenkins # jenkins/jenkins -> your image if you pushed it
+
+# Run container after reboot
+cat <<EOF > /home/ubuntu/run_container.sh
+#!/bin/bash
+docker start jenkins-master
+EOF
+chmod +x /home/ubuntu/run_container.sh
+echo "@reboot /home/ubuntu/run_container.sh" >> /var/spool/cron/crontabs/root
